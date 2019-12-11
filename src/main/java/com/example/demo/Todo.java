303a501d6142b0a387617f8427d57dccdb1d1cc5
RoutinePoutine <matthew.robert.leroe@gmail.com>
2019-12-11 17:05:08 -0500

    Need to Review End of Class Not Turned In

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String task_name;
+
+    @NotNull
+    @Min(2019)
+    private int due_date;
+
+    @NotNull
+    @Min(1)
+    private int priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
+
+    public int getDue_date() {
+        return due_date;
+    }
+
+    public void setDue_date(int due_date) {
+        this.due_date = due_date;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
